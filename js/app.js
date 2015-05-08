21334ac5fda55354c8f2175c1d5ba5571ecb52c3
Florent Espanet <florent.esp@gmail.com>
2015-05-08 16:22:56 +0200

    Add tags and tag filters.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,28 @@
 var app = angular.module("app", []);
 
-app.filter("shuffle", function() {
-    return (function(array) {
-	/* http://stackoverflow.com/a/12646864/2437219 */
-	for (var i = array.length - 1; i > 0; i--) {
-	    var j = Math.floor(Math.random() * (i + 1));
-	    var temp = array[i];
-	    array[i] = array[j];
-	    array[j] = temp;
+app.filter("filterByTags", function() {
+    return (function(array, tags) {
+	console.log("filterByTags", tags);
+	var filtered = [];
+
+	if (tags === undefined || !tags || tags.length === 0) {
+	    return array;
 	}
-	return array;
+	for (var i = 0; i < array.length; i++) {
+	    if (array[i].tags) {
+		var valid = true;
+		for (var ti = 0; ti < tags.length; ti++) {
+		    if (array[i].tags.indexOf(tags[ti]) === -1) {
+			valid = false;
+			break;
+		    }
+		}
+		if (valid) {
+		    filtered.push(array[i]);
+		}
+	    }
+	}
+	return filtered;
     });
 });
 
@@ -17,9 +30,31 @@ app.controller("IndexCtrl", function ($scope, $http) {
     (function () {
 	$scope.developers = [];
 	$scope.error = false;
+	$scope.tags = [];
+
+	$scope.toggleTag = function (tag) {
+	    var index = $scope.tags.indexOf(tag);
+
+	    if (index !== -1) {
+		$scope.tags.splice(index, 1);
+	    } else {
+		$scope.tags.push(tag);
+	    }
+	};
+
+	var shuffle = function (array) {
+	    /* http://stackoverflow.com/a/12646864/2437219 */
+	    for (var i = array.length - 1; i > 0; i--) {
+		var j = Math.floor(Math.random() * (i + 1));
+		var temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	    }
+	    return array;
+	};
 
 	$http.get("developers.json").success(function (response) {
-	    $scope.developers = response.developers;
+	    $scope.developers = shuffle(response.developers);
 	}).error(function (response) {
 	    $scope.error = true;
 	});
